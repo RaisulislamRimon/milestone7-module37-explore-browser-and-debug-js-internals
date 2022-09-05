834105de1afbe8258682c1fee3121202660e5757
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-09-05 07:00:16 +0000

    update

--- module37-6/js/2 interval.js
+++ module37-6/js/2 interval.js
@@ -36,11 +36,21 @@
 //   console.log(seconds++);
 // }, 500);
 
+// let seconds = 0;
+
+// const setIntervalId = setInterval(() => {
+//   seconds++;
+//   console.log(seconds);
+//   if (seconds === 10) {
+//     clearInterval(setIntervalId);
+//   }
+//   //   console.log(seconds++);
+// }, 500);
+
 let seconds = 0;
 
 const setIntervalId = setInterval(() => {
-  seconds++;
-  console.log(seconds);
+  console.log(seconds++);
   if (seconds === 10) {
     clearInterval(setIntervalId);
   }
