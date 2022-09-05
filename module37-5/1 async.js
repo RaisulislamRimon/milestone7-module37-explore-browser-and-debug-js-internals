0db8150664bacfeb0e7d23ca6389d5965168dff7
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-09-05 05:46:03 +0000

    update

--- module37-5/1 async.js
+++ module37-5/1 async.js
@@ -1,6 +1,16 @@
-console.log('first');
-console.log('second');
-console.log('third');
-console.log('fourth');
-console.log('fifth');
+console.log("first");
+console.log("second");
+console.log("third");
+console.log("fourth");
+console.log("fifth");
 
+function doSomething() {
+  console.log("do something");
+  console.log("ninth");
+}
+
+console.log("sixth");
+console.log("seventh");
+console.log("eighth");
+doSomething();
+console.log("tenth");
